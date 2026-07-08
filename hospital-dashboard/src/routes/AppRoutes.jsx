e53f847356e57899c6913dba3718ea4b6dea3dd8
mdtarikkhan87-cyber <mdtarikkhan87@gmail.com>
2026-07-08 13:36:03 +0530

    Fix: restrict driver login, add input validation, remove dead pages

--- hospital-dashboard/src/routes/AppRoutes.jsx
+++ hospital-dashboard/src/routes/AppRoutes.jsx
@@ -4,12 +4,8 @@ import Dashboard from '../pages/Dashboard';
 import ActiveEmergencies from '../pages/ActiveEmergencies';
 import LiveTracking from '../pages/LiveTracking';
 import EmergencyDetails from '../pages/EmergencyDetails';
-import AdminApprovals from '../pages/AdminApprovals';
 import AmbulanceRegistration from '../pages/AmbulanceRegistration';
 import Analytics from '../pages/Analytics';
-import ApprovalManagement from '../pages/ApprovalManagement';
-import DriverApp from '../pages/DriverApp';
-import DriverRegistration from '../pages/DriverRegistration';
 import DashboardLayout from '../layouts/DashboardLayout';
 import ProtectedRoute from './ProtectedRoute';
 
@@ -25,12 +21,8 @@ function AppRoutes() {
         }
       >
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/drivers/register" element={<DriverRegistration />} />
         <Route path="/ambulances/register" element={<AmbulanceRegistration />} />
-        <Route path="/approvals" element={<ApprovalManagement />} />
         <Route path="/analytics" element={<Analytics />} />
-        <Route path="/admin/approvals" element={<AdminApprovals />} />
-        <Route path="/driver" element={<DriverApp />} />
         <Route path="/emergencies" element={<ActiveEmergencies />} />
         <Route path="/tracking" element={<LiveTracking />} />
         <Route path="/emergency/:id" element={<EmergencyDetails />} />
