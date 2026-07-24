7b3eadb24b1a9ebc0bb1f29f233f354d13b9849b
amishh07 <amishranjan638@gmail.com>
2026-07-24 18:13:45 +0530

    Delete hospital-dashboard directory

--- hospital-dashboard/src/mock/mockHospitals.js
+++
@@ -1,5 +0,0 @@
-// Mock hospitals list
-export const mockHospitals = [
-  { id: 'HSP01', name: 'Pune Smart Emergency Hospital', location: { latitude: 18.5204, longitude: 73.8567 }, capacity: { icu: 4, beds: 120 } },
-  { id: 'HSP02', name: 'City General Hospital', location: { latitude: 18.5300, longitude: 73.8450 }, capacity: { icu: 2, beds: 86 } },
-];
